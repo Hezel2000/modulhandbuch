dbc9ec4ab8ac33106034795c3b2906e9e77c68a8
Dominik <dominik.hezel@em.uni-frankfurt.de>
2025-01-06 12:25:38 +0100

    test

--- pages/Suche.py
+++ pages/Suche.py
@@ -28,77 +28,80 @@ search_query = st.text_input("Sucheingabe", placeholder='Sucheingabe', label_vis
 
 if search_query:
     results = search_json_files(data, search_query)
-    st.write("Suchergebnisse:")
     for file_name, json_data in results.items():
         module_content = json_data
-        module_name = module_content['Titel'][0] + ': ' + module_content['Titel'][2]
+        module_name = module_content['Modul-Code'] + ': ' + module_content['Modul-Name-de']
         
         with st.expander(module_name, expanded=False):
-            st.title(module_content['Titel'][0] + ': ' + module_content['Titel'][2])
+            st.title(module_content['Modul-Code'] + ': ' + module_content['Modul-Name-de'])
 
             col1, col2, col3, col4 = st.columns(4)
 
             with col1:
-                st.write(module_content['Titel'][1])
+                st.write(module_content['Modul-Name-de'])
 
             with col2:
-                st.write(module_content['Titel'][3])
+                st.write(module_content['Modul-Typ'])
 
             with col3:
-                st.write(module_content['Titel'][7])
+                st.write(module_content['Anzahl SWS'])
 
             with col4:
-                st.write(module_content['Titel'][4])
+                st.write(module_content['Anzhal CP und Arbeitsaufwand'])
 
-            st.write(module_content['Titel'][5], ' - & - ', module_content['Titel'][6])
-           
-            st.write("**Inhalte**")
-            st.write(module_content["Inhalte"])
-            st.write("**Lernergebnisse / Kompetenzziele**")
-            st.write(module_content["Lernergebnisse / Kompetenzziele"])
+            st.markdown(f'Kontaktstudium {module_content["Kontaktstudium"]}  & Selbststudium {module_content["Selbststudium"]}')
+            st.markdown(f''':blue[**Inhalte**]\n {module_content["Inhalte"]}''')
+            st.markdown(f''':blue[**Lernergebnisse / Kompetenzziele**]\n {module_content["Lernergebnisse / Kompetenzziele"]}''')
+            st.markdown(f''':blue[**Teilnahmevoraussetzungen für Modul bzw. für einzelne Lehrveranstaltungen des Moduls:**]\n {module_content["Teilnahmevoraussetzungen für Modul bzw. für einzelne Lehrveranstaltungen des Moduls"]}''')
+            st.markdown(f''':blue[**Empfohlene Voraussetzungen**] \n {module_content["Empfohlene Voraussetzungen"]}''')
+            st.markdown(f''':blue[**Organisatorische Hinweise**]\n {module_content["Organisatorische Hinweise"]}''')
+            st.markdown(f''':blue[**Zuordnung des Moduls (Studiengang / Fachbereich)**]\n {module_content["Zuordnung des Moduls (Studiengang / Fachbereich)"]}''')
+            st.markdown(f''':blue[**Verwendbarkeit des Moduls für andere Studiengänge**]\n {module_content["Verwendbarkeit des Moduls für andere Studiengänge"]}''')
+            st.markdown(f''':blue[**Häufigkeit des Angebots**]\n {module_content["Häufigkeit des Angebots"]}''')
+            st.markdown(f''':blue[**Dauer des Moduls**]\n {module_content["Dauer des Moduls"]}''')
+            st.markdown(f''':blue[**Modulbeauftragte / Modulbeauftragter:**] {module_content["Modulbeauftragte / Modulbeauftragter"]}''')
+            st.markdown(f"""
+            <div style="color: #4a90e2; font-weight: bold; margin-bottom: 10px;">Studiennachweise/ ggf. als Prüfungsvorleistungen</div>
+            <ul style="list-style-type: none; margin-left: 0; padding-left: 0;">
+                <li>
+                    <span style="color: green; font-weight: bold;">Teilnahmenachweise:</span> {module_content["Studiennachweise/ ggf. als Prüfungsvorleistungen"]["Teilnahmenachweise"]}
+                </li>
+                <li>
+                    <span style="color: green; font-weight: bold;">Leistungsnachweise:</span> {module_content["Studiennachweise/ ggf. als Prüfungsvorleistungen"]["Leistungsnachweise"]}
+                </li>
+            </ul>
+            """, unsafe_allow_html=True)
+            st.markdown(f''':blue[**Lehr- / Lernformen:**] {module_content["Lehr- / Lernformen"]}''')
+            st.markdown(f''':blue[**Unterrichts- / Prüfungssprache:**] {module_content["Unterrichts- / Prüfungssprache"]}''')
+            st.markdown(f"""
+            <div style="color: #4a90e2; font-weight: bold; margin-bottom: 10px;">Modulprüfung</div>
+            <ul style="list-style-type: none; margin-left: 0; padding-left: 0;">
+                <li>
+                    <span style="color: green; font-weight: bold;">Modulabschlussprüfung bestehend aus</span> {module_content["Modulprüfung"]["Modulabschlussprüfung bestehend aus"]}
+                </li>
+                <li>
+                    <span style="color: green; font-weight: bold;">kumulative Modulprüfung bestehend aus</span> {module_content["Modulprüfung"]["kumulative Modulprüfung bestehend aus"]}
+                </li>
+                <li>
+                    <span style="color: green; font-weight: bold;">Bildung der Modulnote bei kumulativen Modulprüfungen</span> {module_content["Modulprüfung"]["Bildung der Modulnote bei kumulativen Modulprüfungen"]}
+                </li>
+            </ul>
+            """, unsafe_allow_html=True)
+                        
+            # st.table(module_content['Übersicht'])
 
-            st.write('**Teilnahmevoraussetzungen für Modul bzw. für einzelne Lehrveranstaltungen des Moduls:**', ' ', module_content['Teilnahmevoraussetzungen für Modul bzw. für einzelne Lehrveranstaltungen des Moduls'])
-            st.write('**Empfohlene Voraussetzungen:**', ' ', module_content['Empfohlene Voraussetzungen'])
-            st.write('**Organisatorische Hinweise:**', ' ', module_content['Organisatorische Hinweise'])
-            st.write('**Zuordnung des Moduls (Studiengang / Fachbereich):**', ' ', module_content['Zuordnung des Moduls (Studiengang / Fachbereich)'])
-            st.write('**Verwendbarkeit des Moduls für andere Studiengänge:**', ' ', module_content['Verwendbarkeit des Moduls für andere Studiengänge'])
-            st.write('**Häufigkeit des Angebots:**', ' ', module_content['Häufigkeit des Angebots'])
-            st.write('**Dauer des Moduls:**', ' ', module_content['Dauer des Moduls'])
-            st.write('**Modulbeauftragte / Modulbeauftragter:**', ' ', module_content['Modulbeauftragte / Modulbeauftragter'])
-            st.write('**Studiennachweise/ ggf. als Prüfungsvorleistungen:**')
-            Teilnahmenachweise_coloured = {'colored_text': '<span style="color:#0077be;">&nbsp;&nbsp;&nbsp;&nbsp;*Teilnahmenachweise*</span>',
-                    'additional_text': module_content['Studiennachweise/ ggf. als Prüfungsvorleistungen']['Teilnahmenachweise']}
-            st.write(Teilnahmenachweise_coloured['colored_text'], Teilnahmenachweise_coloured['additional_text'], unsafe_allow_html=True)
-            Leistungsnachweise_coloured = {'colored_text': '<span style="color:#0077be;">&nbsp;&nbsp;&nbsp;&nbsp;*Leistungsnachweise*</span>',
-                    'additional_text': module_content['Studiennachweise/ ggf. als Prüfungsvorleistungen']['Leistungsnachweise']}
-            st.write(Leistungsnachweise_coloured['colored_text'], Leistungsnachweise_coloured['additional_text'], unsafe_allow_html=True)
-            st.write('**Lehr- / Lernformen:**', ' ', module_content['Lehr- / Lernformen'])
-            st.write('**Unterrichts- / Prüfungssprache:**', ' ', module_content['Unterrichts- / Prüfungssprache'])
-            st.write('**Modulprüfung:**')
-            modulabschluss_coloured = {'colored_text': '<span style="color:#0077be;">&nbsp;&nbsp;&nbsp;&nbsp;*Modulabschlussprüfung bestehend aus:*</span>',
-                    'additional_text': module_content['Modulprüfung']['Modulabschlussprüfung bestehend aus:']}
-            st.write(modulabschluss_coloured['colored_text'], modulabschluss_coloured['additional_text'], unsafe_allow_html=True)
-            kumulative_coloured = {'colored_text': '<span style="color:#0077be;">&nbsp;&nbsp;&nbsp;&nbsp;*kumulative Modulprüfung bestehend aus:*</span>',
-                    'additional_text': module_content['Modulprüfung']['kumulative Modulprüfung bestehend aus:']}
-            st.write(kumulative_coloured['colored_text'], kumulative_coloured['additional_text'], unsafe_allow_html=True)
-            modulnote_coloured = {'colored_text': '<span style="color:#0077be;">&nbsp;&nbsp;&nbsp;&nbsp;*Bildung der Modulnote bei kumulativen Modulprüfungen:*</span>',
-                    'additional_text': module_content['Modulprüfung']['Bildung der Modulnote bei kumulativen Modulprüfungen:']}
-            st.write(modulnote_coloured['colored_text'], modulnote_coloured['additional_text'], unsafe_allow_html=True)
 
-            st.table(module_content['Übersicht'])
-
-
-# ---------------------
+# # ---------------------
             
-            st.divider()
-            st.header('Bemerkungen')
+#             st.divider()
+#             st.header('Bemerkungen')
 
-            st.write('**Prüfung:**', ' ', module_content['Prüfung'])
+#             st.write('**Prüfung:**', ' ', module_content['Prüfung'])
             
-            st.divider()
-            col1, col2 = st.columns([30,70])
-            with col1:
-                sel_version = st.selectbox('Version', module_content['Version'], key=module_name)
-            with col2:
-                st.write(module_content['Änderung'][sel_version])
+#             st.divider()
+#             col1, col2 = st.columns([30,70])
+#             with col1:
+#                 sel_version = st.selectbox('Version', module_content['Version'], key=module_name)
+#             with col2:
+#                 st.write(module_content['Änderung'][sel_version])
    
