b52215dcab59be5f39e8a638cadcf6dda65229d4
Dominik <dominik.hezel@em.uni-frankfurt.de>
2025-01-06 14:10:41 +0100

    test

---
+++ pages/02_Überblick.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write('Rechts oben neben dem Bild kann für eine vergrößerte Darstellung geklickt werden. Der dazu gehörige Doppelpfeil erscheint, sobald sich die Maus über dem Bild befindet.')
+st.image("images/BA Übersicht.png", caption='BSc')
+st.image("images/MA Übersicht I.png", caption='MSc')
